fb3dce81ce519efaf6d03c3b380022034e21a3d6
Marc Cooperstein <mcooperstein@gmail.com>
2016-07-27 10:37:38 -0700

    added review and jQuery slider for reviews section
    added review and jQuery slider for reviews section

--- js/creative.js
+++ js/creative.js
@@ -4,11 +4,11 @@
  * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap/blob/gh-pages/LICENSE)
  */
 
-(function($) {
+(function ($) {
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
+    $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
@@ -23,7 +23,7 @@
     })
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
+    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function () {
         $('.navbar-toggle:visible').click();
     });
 
@@ -73,6 +73,12 @@
         image: {
             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
         }
+    });
+
+    //Added jQuery Image Slider
+    $('.bxslider').bxSlider({
+        mode: 'fade',
+        captions: true
     });
 
 })(jQuery); // End of use strict
